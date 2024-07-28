f19508eae43c852086348a8fc5bc9de6775b40b0
Fernando Aranha <ferspider3@hotmail.com>
2024-07-28 15:34:23 -0400

    Corrigido functions.php
    Corrigido bug de validação do SQL

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,26 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+function redirect_if_not_logged_in() {
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: login.php');
+        exit();
+    }
+}
+
+if (!function_exists('contains_sql_commands')) {
+    function contains_sql_commands($value) {
+        $sql_commands = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'TRUNCATE', 'ALTER', 'CREATE', 'RENAME'];
+        foreach ($sql_commands as $command) {
+            if (stripos($value, $command) !== false) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
 function is_logged_in() {
     return isset($_SESSION['user_id']);
@@ -7,13 +28,6 @@ function is_logged_in() {
 
 function is_admin() {
     return is_logged_in() && $_SESSION['access_level'] === 'admin';
-}
-
-function redirect_if_not_logged_in() {
-    if (!is_logged_in()) {
-        header('Location: login.php');
-        exit();
-    }
 }
 
 function redirect_if_not_admin() {
