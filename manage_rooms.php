fece5315573908e49d3075dc7559523ffdbd51d2
Fernando Aranha <ferspider3@hotmail.com>
2024-08-05 22:07:20 -0400

    Atualizado manage_rooms.php
    Atualizado alguns bugs de visualização

--- manage_rooms.php
+++ manage_rooms.php
@@ -26,11 +26,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 $rooms = $pdo->query("SELECT * FROM rooms")->fetchAll();
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="pt-br" data-bs-theme="dark">
 <head>
     <title>Gerenciar Salas</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="images/favicon16.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 <div class="d-flex">
